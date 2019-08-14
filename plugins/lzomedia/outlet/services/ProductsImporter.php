429f1feece140f50bfbafa841006e90a509c93f5
Talnaci Alexandru <flafyrex@gmail.com>
2019-08-14 03:34:41 +0300

    Product Import with Preview Images(gallery BUG)

--- plugins/lzomedia/outlet/services/ProductsImporter.php
+++ plugins/lzomedia/outlet/services/ProductsImporter.php
@@ -8,6 +8,7 @@ use Lovata\Shopaholic\Classes\Import\ImportOfferModelFromCSV;
 use Lovata\Shopaholic\Classes\Import\ImportProductModelFromCSV;
 use Lovata\Shopaholic\Models\Category;
 use LzoMedia\Outlet\Contracts\ImporterInterface;
+use Illuminate\Support\Facades\Storage;
 
 class ProductsImporter extends ImportProductModelFromCSV
 {
@@ -20,9 +21,7 @@ class ProductsImporter extends ImportProductModelFromCSV
 
     public function __construct($json)
     {
-
         $this->json = $json;
-
     }
 
     /**
@@ -31,10 +30,12 @@ class ProductsImporter extends ImportProductModelFromCSV
      */
     private function getItem()
     {
-        return $this->item =[
+        return $this->item = [
             'name' => $this->json->name,
             'external_id' => $this->json->id,
             'code' => $this->json->offer->vendor->name->display,
+            'preview_image' => $this->json->image->original,
+            'images' => $this->json->image_gallery,
             'active' => true,
             'category_id' => 2,
             'brand_id' => 1
@@ -45,17 +46,10 @@ class ProductsImporter extends ImportProductModelFromCSV
      * @throws \Throwable
      */
     public function process(){
-
-
         $this->prepareImportData();
-
         parent::import($this->getItem(), false);
-
         return $this;
-
     }
-
-
 
     /**
      * Prepare array of import data
@@ -66,14 +60,70 @@ class ProductsImporter extends ImportProductModelFromCSV
         parent::setBrandField();
         $this->setCategoryField();
 
-        parent::initPreviewImage();
-        parent::initImageList();
+        $this->initPreviewImage();
+        $this->initImageList();
         parent::initAdditionalCategoryField();
 
         parent::prepareImportData();
     }
 
+    protected function initImageList()
+    {
+        if (!array_key_exists('images', $this->arImportData)) {
+            $this->bNeedUpdateImageList = false;
+            return;
+        }
 
+        $this->bNeedUpdateImageList = true;
+        $ImageList = array_get($this->arImportData, 'images');
+        array_forget($this->arImportData, 'images');
+
+        if (empty($ImageList)) {
+            return;
+        }
+
+        $this->arImageList = array();
+
+        foreach ($ImageList as $idx => $img) {
+            $im = $img->original;
+            $extension = array_values(array_slice(explode(".", $im), -1))[0];
+
+            $filename = array_get($this->arImportData, 'external_id') . '_' . $idx . '.' . $extension;
+
+            $imgFP = storage_path('app/imgstorage/' . $filename);
+            if (!file_exists($imgFP)) {
+                Storage::put('imgstorage/' . $filename, file_get_contents($im));
+            }
+            $this->arImageList[$idx] = $imgFP;
+        }
+    }
+
+    /**
+     * Init preview image path
+     */
+    protected function initPreviewImage()
+    {
+        if (!array_key_exists('preview_image', $this->arImportData)) {
+            $this->bNeedUpdatePreviewImage = false;
+            return;
+        }
+
+        $this->bNeedUpdatePreviewImage = true;
+        $this->sPreviewImage = array_get($this->arImportData, 'preview_image');
+        if ($this->sPreviewImage == null) {
+            return;
+        }
+
+        $extension = array_values(array_slice(explode(".", $this->sPreviewImage), -1))[0];
+        $filename = array_get($this->arImportData, 'external_id') . '_preview.' . $extension;
+
+        $imgFP = storage_path('app/imgstorage/' . $filename);
+
+        if (!file_exists($imgFP)) {
+            Storage::put('imgstorage/' . $filename, file_get_contents($this->sPreviewImage));
+        }
+        $this->sPreviewImage = $imgFP;
+    }
 
     /**
      * @method setCategoryField
